0e68af1907c954982f015dbe6015629148f4fb42
mitsuru793 <mitsuru793@gmail.com>
2019-05-31 02:34:15 +0900

    modify test library from mocha to jest

--- src/__tests__/UserTest.ts
+++ src/__tests__/UserTest.ts
@@ -1,11 +1,10 @@
-import { assert } from 'chai'
-import User from 'User'
+import User from "../User"
 
 describe('User', () => {
   describe('constructor', () => {
     it('can do', () => {
       const user = new User()
-      assert.instanceOf(user, User)
+      expect(user).toBeInstanceOf(User)
     })
   })
 })
